1d3eab62a4507530ee625ae18bcdec0111709fa0
Trieu Le <leviettrieu369@gmail.com>
2020-10-09 03:12:27 +0700

    Get Location by easier method. Error at headless run

--- WindowsCampApplication/Form1.cs
+++ WindowsCampApplication/Form1.cs
@@ -18,12 +18,13 @@ using OpenQA.Selenium;
 using System.Text.RegularExpressions;
 using System.Collections.Concurrent;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace WindowsCampApplication
 {
     public partial class webCampingWindows : Form
     {
-        
+
         public static String[] userAgent = new String[] {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.87 Safari/537.36",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36",
@@ -74,11 +75,11 @@ namespace WindowsCampApplication
                     {
                         fileContent = reader.ReadToEnd();
                         sub_array = fileContent.Split('\n');
-                        
+
                     }
 
                     // Add order to array
-                    foreach(String s in sub_array)
+                    foreach (String s in sub_array)
                     {
                         OrderInfo sub_order = new OrderInfo();
                         String[] info = s.Split('|');
@@ -101,10 +102,10 @@ namespace WindowsCampApplication
             {
                 String message = "Please insert number of tab";
                 MessageBoxButtons buttons = MessageBoxButtons.OK;
-                MessageBox.Show(message, "Alert message",buttons,MessageBoxIcon.Warning);
+                MessageBox.Show(message, "Alert message", buttons, MessageBoxIcon.Warning);
                 return;
             }
-            if (!tabBox.Text.All(c=>Char.IsNumber(c)))
+            if (!tabBox.Text.All(c => Char.IsNumber(c)))
             {
                 String message = "Tab should be number";
                 MessageBoxButtons buttons = MessageBoxButtons.OK;
@@ -112,7 +113,7 @@ namespace WindowsCampApplication
                 return;
             }
             TAB = Int32.Parse(tabBox.Text);
-            if (TAB >= 5||TAB <= 0)
+            if (TAB >= 5 || TAB <= 0)
             {
                 //alert box
                 String message = "Number of tab should be over 0 and under 5";
@@ -121,8 +122,8 @@ namespace WindowsCampApplication
                 return;
             }
             DateTime now = DateTime.Now;
-            
-            if(orderList.Count == 0)
+
+            if (orderList.Count == 0)
             {
                 String message = "Need add order";
                 MessageBoxButtons buttons = MessageBoxButtons.OK;
@@ -130,6 +131,7 @@ namespace WindowsCampApplication
             }
             else
             {
+                //LoadDriver(orderList[2]);
                 Parallel.ForEach(orderList,
                     // Limit load page per time
                     new ParallelOptions { MaxDegreeOfParallelism = TAB }, order =>
@@ -138,7 +140,7 @@ namespace WindowsCampApplication
                         Console.WriteLine("Link: {0}, at Thread = {1}",
                             order.OrderLink,
                             Thread.CurrentThread.ManagedThreadId);
-                        Thread.Sleep(10000);
+                        Thread.Sleep(15000);
                     }
                 );
             }
@@ -150,15 +152,22 @@ namespace WindowsCampApplication
             var chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
             ChromeOptions options = new ChromeOptions();
-            
+
             // set up agent
             Random rand = new Random();
             int agent = rand.Next(0, userAgent.Length);
             options.AddArgument("--user-agent=" + userAgent[agent]);
-            //options.AddArguments("--proxy-server=http://" + "75.109.186.174:8080");
             options.AddArguments("--disable-gpu");
+            //options.AddArguments("--no-sandbox");
+            //options.AddArguments("--silent");
+            //options.AddArguments("--disable-web-security");
             options.AddArguments("--disable-extensions");
-            options.AddArgument("ignore-certificate-errors");
+            //options.AddArguments("--log-level=3");
+            //options.AddArguments("--proxy-server='direct://'");
+            //options.AddArguments("--proxy-bypass-list=*");
+            //options.AddArgument("test-type");
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddArguments("--allow-running-insecure-content");
             if (HEADLESS == 1)
             {
                 options.AddArguments("--incognito", "--headless");
@@ -172,42 +181,55 @@ namespace WindowsCampApplication
             IWebDriver driver = new ChromeDriver(chromeDriverService, options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            driver.Navigate().GoToUrl(orderInfo.OrderLink);
-            Console.WriteLine("Loaded page");
+
+            driver.Navigate().GoToUrl("https://www.nike.com/");
+            Console.WriteLine("Loaded NIKE page");
+            Thread.Sleep(15000);
+
+            // get location
+            driver.FindElement(By.XPath("//a[@class='fs10-nav-sm nav-color-white country-pin']")).Click();
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//p[@class='nav-bold' and contains(text(),'United States')]")).Click();
+            Thread.Sleep(2000);
+
+            driver.Navigate().GoToUrl("https://www.nike.com/launch/");
+            Console.WriteLine("Loaded NIKE Launch page");
             Thread.Sleep(10000);
-            string locate = "";
-            try
-            {
-                locate = driver.FindElement(
-                    By.XPath("//span[@class='d-sm-ib va-sm-m small text-color-secondary']")).Text;
-                Thread.Sleep(2000);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                driver.Quit();
-            }
-            if (!locate.Equals(orderInfo.Country))
-            {
-                driver.FindElement(By.XPath(
-                "//button[@data-qa='locale-selector-button']")).Click();
-                Thread.Sleep(5000);
+            //string locate = "";
+            //try
+            //{
+            //    locate = driver.FindElement(
+            //        By.XPath("//span[@class='d-sm-ib va-sm-m small text-color-secondary']")).Text;
+            //    Thread.Sleep(2000);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //    driver.Quit();
+            //}
+            //if (!locate.Equals(orderInfo.Country))
+            //{
+            //    driver.FindElement(By.XPath(
+            //    "//button[@data-qa='locale-selector-button']")).Click();
+            //    Thread.Sleep(5000);
 
-                driver.FindElement(By.XPath(
-                    "//div[@class='ncss-container p6-sm p12-md u-full-width u-full-height']"));
-                Thread.Sleep(2000);
-                Console.WriteLine("Load select location");
+            //    driver.FindElement(By.XPath(
+            //        "//div[@class='ncss-container p6-sm p12-md u-full-width u-full-height']"));
+            //    Thread.Sleep(2000);
+            //    Console.WriteLine("Load select location");
 
-                driver.FindElement(By.XPath(
-                    $"//span[contains(text(), '{orderInfo.Country}')]")).Click();
-                Thread.Sleep(2000);
-                Console.WriteLine("Load click location");
+            //    driver.FindElement(By.XPath(
+            //        $"//span[contains(text(), '{orderInfo.Country}')]")).Click();
+            //    Thread.Sleep(2000);
+            //    Console.WriteLine("Load click location");
 
-                driver.Navigate().GoToUrl(orderInfo.OrderLink);
-                Thread.Sleep(2000);
-                Console.WriteLine("Load page");
+            //}
 
-            }
+            // Load item page
+            driver.Navigate().GoToUrl(orderInfo.OrderLink);
+            Console.WriteLine($"Load page {orderInfo.OrderLink}");
+            Thread.Sleep(10000);
 
             // Check sold out
             bool soldOut = false;
@@ -266,28 +288,41 @@ namespace WindowsCampApplication
                     Thread.Sleep(2000);
 
                     // Click add to cart
-                    driver.FindElement(By.XPath("//button[@class='ncss-btn-primary-dark btn-lg']")).Click();
+                    driver.FindElement(By.XPath("//button[@data-qa='add-to-cart']")).Click();
                     Thread.Sleep(2000);
-
+                    IWebElement CC = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//button[@class='ncss-btn-primary-dark']")));
                     // CLick the cart
-                    driver.FindElement(
-                           By.XPath("//a[@class='hover-color-black text-color-grey bg-transparent " +
+                    if (HEADLESS == 1)
+                    {
+                        Thread.Sleep(2000);
+                        CC.Click();
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        driver.FindElement(
+                           By.XPath("//a[" +
+                           "@class='hover-color-black text-color-grey bg-transparent " +
                            "prl3-sm pt2-sm pb2-sm m0-sm fs12-sm d-sm-b jewel-cart-container']")).Click();
-                    Thread.Sleep(2000);
+                        Thread.Sleep(2000);
+                    }
+                    Console.WriteLine("Load the cart");
                     
+
                     // Click to checkout
                     try
                     {
                         driver.FindElement(
                            By.XPath("//button[@data-automation='guest-checkout-button']")).Click();
                         Thread.Sleep(2000);
-                    }catch(Exception e)
+                    }
+                    catch (Exception e)
                     {
                         Console.WriteLine("There are no product in cart");
                     }
-                    
+
                     driver.Quit();
-                    Console.WriteLine($"Load finish {orderInfo.OrderLink}");
+                    Console.WriteLine($"Load finish {orderInfo.OrderLink} SUCCESSED");
                 }
             }
         }
